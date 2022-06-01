a72280655be18ae3b229e73b5b9d326893894e08
Nyrzak <oleg.polov75@gmail.com>
2022-06-01 23:33:50 +0300

    practice 6. settings added

---
+++ MireaProject/app/src/main/java/ru/mirea/yagoda/myapplication/ui/Settings/HomeSettingsFragment.java
@@ -0,0 +1,82 @@
+package ru.mirea.yagoda.myapplication.ui.Settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+import ru.mirea.yagoda.myapplication.R;
+
+public class HomeSettingsFragment extends Fragment {
+    private TextView textView;
+    private EditText editText;
+    private Button applyTextButton;
+    private Button saveButton;
+    private Switch switch1;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String SWITCH1 = "switch1";
+
+    private String text;
+    private boolean switchOnOff;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home_settings, null);
+
+        textView = (TextView) v.findViewById(R.id.textview);
+        editText = (EditText) v.findViewById(R.id.edittext);
+        applyTextButton = (Button) v.findViewById(R.id.apply_text_button);
+        saveButton = (Button) v.findViewById(R.id.save_button);
+        switch1 = (Switch) v.findViewById(R.id.switch1);
+
+        applyTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+
+        loadData();
+        updateViews();
+
+        return v;
+    }
+    public void saveData() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, textView.getText().toString());
+        editor.putBoolean(SWITCH1, switch1.isChecked());
+
+        editor.apply();
+
+        Toast.makeText(getActivity(), "Data saved", Toast.LENGTH_SHORT).show();
+    }
+    public void loadData() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+        switchOnOff = sharedPreferences.getBoolean(SWITCH1, false);
+    }
+    public void updateViews() {
+        textView.setText(text);
+        switch1.setChecked(switchOnOff);
+    }
+}
